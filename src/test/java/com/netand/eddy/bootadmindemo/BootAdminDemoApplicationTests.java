1c796fafd629f82ee3f6128f421f418485ee848e
bom1boyy <121221729+bom1boyy@users.noreply.github.com>
2024-06-04 15:53:51 +0900

    Update BootAdminDemoApplicationTests.java

--- src/test/java/com/netand/eddy/bootadmindemo/BootAdminDemoApplicationTests.java
+++ src/test/java/com/netand/eddy/bootadmindemo/BootAdminDemoApplicationTests.java
@@ -3,11 +3,20 @@ package com.netand.eddy.bootadmindemo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 @SpringBootTest
 class BootAdminDemoApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void 테스트용(){
+		int a = 1;
+		int b = 2;
+		assertEquals( 3, a+b );
 	}
 
 }
